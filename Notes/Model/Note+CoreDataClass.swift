2db828d7daf73731275e383cafd19c6201e9721d
Ahmed Abd Elaziz <ahmedabdelaziz500@gmail.com>
2021-10-01 15:51:43 +0200

    Create  NoteCoreDataClass

---
+++ Notes/Model/Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  Notes
+//
+//  Created by Ahmed Abd Elaziz on 01/10/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}
