fd083f556248b21b7eb5cd8aecc97a57482dcff9
Ahmed Abd Elaziz <ahmedabdelaziz500@gmail.com>
2021-10-01 15:44:04 +0200

    Create MyNotesView for UI

---
+++ Notes/View/MyNotes/MyNotesView.swift
@@ -0,0 +1,22 @@
+//
+//  MyNotesView.swift
+//  Notes
+//
+//  Created by Ahmed Abd Elaziz on 01/10/2021.
+//
+
+import Foundation
+import UIKit
+
+class MyNotesView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
